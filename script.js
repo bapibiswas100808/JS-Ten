dde7447c5150c696884ecea7f2031eb855b8a6f5
Bapi Biswas <star.bappy@gmail.com>
2023-01-09 18:30:19 +0600

    homework done

---
+++ script.js
@@ -0,0 +1,69 @@
+// query selector
+// const tag1 = document.querySelector("h1");
+// console.log(tag1);
+
+// // example
+// const class1 = document.querySelector(".class1");
+// console.log(class1);
+// // example
+// const id1 = document.querySelector("#id1");
+// console.log(id1);
+
+// query selector all
+// const tag1 = document.querySelectorAll("h1");
+// console.log(tag1);
+
+// const tag2 = document.getElementsByTagName("h1");
+// console.log(tag2);
+
+// Child Nodes
+// const id1 = document.getElementById("id1");
+// console.log(id1);
+// console.log(id1.childNodes);
+
+// Dom element children
+// const id1 = document.getElementById("list-id");
+// console.log(id1);
+// console.log(id1.children);
+
+// // creat element
+// const newList = document.createElement("li");
+// console.log(newList);
+// newList.innerHTML = "List 5";
+
+// // Appen Child
+// const listid = document.getElementById("list-id");
+// listid.appendChild(newList);
+
+// example
+// const id1 = document.getElementById("div-text");
+// const newHeading = document.createElement("h2");
+// newHeading.innerText = "This is heading Two";
+// id1.appendChild(newHeading);
+
+// Remove child
+// const id1 = document.getElementById("list-id");
+// console.log(id1);
+// const list = id1.children[0];
+// console.log(list);
+// id1.removeChild(list);
+// console.log(id1.children);
+
+// replace child
+// const id1 = document.getElementById("list-id");
+// const list2 = id1.children[1];
+// const newlist = document.createElement("li");
+// newlist.innerText = "this is newlist";
+// id1.replaceChild(newlist, list2);
+
+// title
+// console.log(document.title);
+// document.title = "New Java script title";
+
+// body
+// console.log(document.body);
+// document.body.innerHTML = "<h1>Congratulations!</h1>";
+
+// const id1 = document.getElementById("id1");
+// id1.innerHTML = "<p>This is a paragraph<p/>";
+// id1.innerText = "<p>This is a paragraph<p/>";
